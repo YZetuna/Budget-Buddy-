4ea50c03eb378e6b0066af884c391eba4602c598
ImNewToC0de <dangjustin98@gmail.com>
2024-06-08 17:57:13 -0400

    Added a welcome message to the dashboard still needs to be able to talk to database for name

---
+++ javascript/dashboard.js
@@ -0,0 +1,31 @@
+"use strict";
+// Strict command
+// Creating a Date object
+// Hours are in Military
+// Format month, day, year, hrs, mins, secs
+let currentDate = new Date();
+
+// Get the day, month, and year
+let day = currentDate.getDate();
+// Month long is full name of the month
+let month = currentDate.toLocaleString('default', { month: 'long' });
+let year = currentDate.getFullYear();
+
+// Get the current hour
+let hour = currentDate.getHours();
+
+// Determine the time of day message
+let timeOfDayMessage;
+if (hour < 12) {
+    timeOfDayMessage = "Good morning!";
+} else if (hour < 18) {
+    timeOfDayMessage = "Good afternoon!";
+} else {
+    timeOfDayMessage = "Good evening!";
+}
+
+// Get the welcome message element
+let welcomeMessage = document.getElementById("welcomeMessage");
+
+// Set the welcome message text
+welcomeMessage.textContent = "Welcome! Today is " + month + " " + day + ", " + year + ". " + timeOfDayMessage;
