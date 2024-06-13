4d322db7f5e71a74d6cee5217ce2415817a1896f
NicholasPerrrin <93680381+NicholasPerrrin@users.noreply.github.com>
2024-06-13 16:46:29 -0400

    finalized login.html and loginauthenticate.php. User can now login in once account has been created.

--- loginauthenticate.php
+++ loginauthenticate.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
+$username = "root";
+$password = "";
 $database = "budget_buddy";
 
 $conn = new mysqli($servername, $username, $password, $database);
@@ -10,18 +10,18 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
 
 // Query the database to check if the username and password are correct
-$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$sql = "SELECT * FROM users WHERE Username='$username' AND Password='$password'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Login successful
-    session_start();
-    $_SESSION['username'] = $username;
-    header("Location: dashboard.php"); // Redirect to the dashboard or home page
+    //session_start();
+    //$_SESSION['username'] = $username;
+    header("Location: dashboard.html"); // Redirect to the dashboard or home page
     exit();
 } else {
     // Login failed
