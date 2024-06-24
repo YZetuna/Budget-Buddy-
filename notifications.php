938748daeae693b82387356c2df12547e1c27444
NicholasPerrrin <93680381+NicholasPerrrin@users.noreply.github.com>
2024-06-24 18:44:33 -0400

    Removed unused links and updated sql database to final version.

---
+++ notifications.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "budget_buddy";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//ssession.start();
+
+$username = trim($_POST['username']);
+$password = trim($_POST['password']);
+
+// Query the database to check if the username and password are correct
+$sql = "SELECT * FROM users WHERE Username='$username' AND Password='$password'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Login successful
+    /*session_start();*/
+    $_SESSION["Username"] = $username;
+    header("Location: dashboard.php"); // Redirect to the dashboard or home page
+    exit();
+} else {
+    // Login failed
+    header("Location: login.html?error=invalid"); // Redirect back to the login page with an error message
+    exit();
+}
+
+$conn->close();
+?>
--- sql/BudgetBuddyDB.sql
+++ sql/BudgetBuddyDB.sql
@@ -1,114 +1,141 @@
-use master
-go
-drop database BudgetBuddy
-go
-Create database BudgetBuddy
-GO
---Making sure we are creating the tables in the right database
-Use BudgetBuddy
-GO
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 25, 2024 at 12:43 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-create table USERS(
-  UserID integer,
-  -- Attributes
-  Username varchar(25) not null,
-  Email varchar(35) not null,
-  Password varchar(30) not null,
-  -- Constraints
-  constraint user_id_pk primary key (UserID),
-  constraint username_uk unique (Username)
-);
-
-GO
-
-INSERT INTO USERS
-	(UserID, Username, Email, Password)
-Values
--- To add More values here to test them
-(1000, 'Test', 'testSql@gmail.com', 'ThisIsATest')
-
-;
-GO
-
-Create table CATEGORY(
-	CategoryID		integer,
-	-- Attributes
-	UserID			integer,
-	category_name	varchar(40),
-
-	-- Constraints
-	constraint category_id_pk primary key (CategoryID),
-	constraint userid_category_fk foreign key(UserID) references USERS(UserID)
-);
-
-GO
-/*INSERT INTO CATEGORY
-	(CategoryID,UserID,category_name)
-Values
-
-;
-GO*/
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-Create table ACCOUNT(
-	AccountID	integer,
-	-- Attributes
-	UserID		integer,
-	account_name	varchar(40)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-	-- Constraints
-	constraint account_id_pk primary key (AccountID),
-	constraint userid_account_fk foreign key (UserID) references USERS(UserID)
-);
-GO
+--
+-- Database: `budget_buddy`
+--
 
-/*INSERT INTO ACCOUNT
-	(AccountID,UserID,account_name)
-Values
+-- --------------------------------------------------------
 
-;
-GO*/
+--
+-- Table structure for table `account`
+--
 
-Create table TRANSACTIONS(
-	TransactionID	integer,
-	AccountID		integer,
-	CategoryID		integer,
-	UserID			integer,
-	Notes			varchar(60) null,
-	Amount			money not null,
+CREATE TABLE `account` (
+  `AccountID` int(11) NOT NULL,
+  `UserID` int(11) DEFAULT NULL,
+  `account_name` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_nopad_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-	-- Constraints
-	constraint transactions_id_pk primary key (TransactionID),
-	constraint categoryid_transactions foreign key (CategoryID) references CATEGORY(CategoryID),
-	constraint accountid_transactions_fk foreign key (AccountID) references ACCOUNT(AccountID),
-	constraint userid_transactions_fk foreign key (UserID) references USERS(UserID)
-);
-GO
+--
+-- Dumping data for table `account`
+--
 
-/*INSERT INTO TRANSACTIONS
-	(TransactionID, AccountID, CategoryID, UserID, Notes, Amount)
-Values
+INSERT INTO `account` (`AccountID`, `UserID`, `account_name`) VALUES
+(3, NULL, 'perrinnick7');
 
-;*/
+-- --------------------------------------------------------
 
-Create table BUDGET(
-	BudgetID		integer,
-	CategoryID		integer,
-	Income			money null,
-	Expenses		money null,
-	Total			money null,
-	EstimatedProfit	money null,
-	TodaysDate		date not null default GetDate(),
+--
+-- Table structure for table `budget`
+--
 
-	-- Constraints
-	constraint budget_id_pk primary key (BudgetID),
-	constraint categoryid_budget_fk foreign key (CategoryID) references CATEGORY(CategoryID)
-);
-GO
+CREATE TABLE `budget` (
+  `BudgetID` int(11) NOT NULL,
+  `AccountID` int(11) DEFAULT NULL,
+  `Income` decimal(19,2) DEFAULT NULL,
+  `Expenses` decimal(19,2) DEFAULT NULL,
+  `Total` decimal(19,2) DEFAULT NULL,
+  `EstimatedProfit` decimal(19,2) DEFAULT NULL,
+  `DateOfBudget` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*INSERT INTO BUDGET
-	(BudgetID,CategoryID,Income,Date)
-Values
+--
+-- Dumping data for table `budget`
+--
 
+INSERT INTO `budget` (`BudgetID`, `AccountID`, `Income`, `Expenses`, `Total`, `EstimatedProfit`, `DateOfBudget`) VALUES
+(14, 3, 0.00, 0.00, 0.00, 0.00, '2024-06-10'),
+(15, 3, 500.00, 100.00, 400.00, 100.00, '2024-06-03'),
+(17, 3, 0.00, 0.00, 0.00, 0.00, '2024-06-18'),
+(19, 3, 1400.00, 40.00, 1360.00, 340.00, '2024-06-20'),
+(28, 3, 2120.00, 50.00, 2070.00, 517.50, '2024-06-23');
 
-;*/
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(25) NOT NULL,
+  `Email` varchar(35) NOT NULL,
+  `Password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `Username`, `Email`, `Password`) VALUES
+(1000, 'Test', 'testSql@gmail.com', 'ThisIsATest'),
+(1003, 'perrinnick7', 'perrinnick7@gmail.com', 'pass');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`AccountID`),
+  ADD KEY `userid_account_fk` (`UserID`);
+
+--
+-- Indexes for table `budget`
+--
+ALTER TABLE `budget`
+  ADD PRIMARY KEY (`BudgetID`),
+  ADD KEY `categoryid_budget_fk` (`AccountID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `username_uk` (`Username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `AccountID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `budget`
+--
+ALTER TABLE `budget`
+  MODIFY `BudgetID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1004;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
